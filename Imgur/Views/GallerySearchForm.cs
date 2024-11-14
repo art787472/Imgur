c7db4af62e13dd48201be6c29c5b0d9dc3950ca8
Chen Yi-Cheng <art787472@gmail.com>
2024-11-14 13:35:24 +0800

    加入專案檔案。

---
+++ Imgur/Views/GallerySearchForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Imgur.Views
+{
+    public partial class GallerySearchForm : Form
+    {
+        public GallerySearchForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
